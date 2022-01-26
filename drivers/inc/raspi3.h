0c5cacc2fe3d35efe223e7ed421d37dafa193909
Aashvij Shenai <aashvijshenai@gmail.com>
2022-01-26 20:13:51 +0530

    -Added INPUT,OUTPUT defns -Changed 0 to PIN0 for easier ref Signed-off: Aashvij Shenai

--- drivers/inc/raspi3.h
+++ drivers/inc/raspi3.h
@@ -4,7 +4,8 @@
 #define RASPI3_H
 
 // memory mapped I/O base
-#ifndef MIMO_BASE
+//Physical addresses range from 0x3F000000 to 0x3FFFFFFF for peripherals
+#ifndef MMIO_BASE
 #define MMIO_BASE               0x3F000000
 #endif
 
--- drivers/src/main.c
+++ drivers/src/main.c
@@ -2,7 +2,7 @@
 #include "exception_handler.h"
 #include <stddef.h>
 
-
+#define PIN0 0  //Using pin0 to test out basic GPIO functions
 
 int main()
 {
@@ -12,19 +12,19 @@ int main()
     volatile unsigned int * result = NULL;
 
     // set GPIO0 as output
-    gpio_set_fcn(0, 1);
+    gpio_set_fcn(PIN0, GPIO_OUT);
 
     // set GPIO0
-    gpio_set_pin(0);
+    gpio_set_pin(PIN0);
 
     // get state of GPIO0
-    gpio_get_pin_state(0, result);
+    gpio_get_pin_state(PIN0, result);
 
     // clear GPIO0
-    gpio_clear_pin(0);
+    gpio_clear_pin(PIN0);
 
     // get state of GPIO0
-    gpio_get_pin_state(0, result);
+    gpio_get_pin_state(PIN0, result);
     
     while(1)
     {
