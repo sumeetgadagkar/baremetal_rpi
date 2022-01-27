c505ddd9285b1cccf26252a81bb23de5fa107ca1
Aashvij Shenai <aashvijshenai@gmail.com>
2022-01-27 11:47:23 +0530

    updated main.c with gpio change -Aashvij Shenai

--- drivers/src/main.c
+++ drivers/src/main.c
@@ -11,7 +11,7 @@ int main()
     volatile unsigned int * result = NULL;
 
     // set GPIO0 as output
-    gpio_set_fcn(PIN0, GPIO_OUT);
+    gpio_set_fcn(PIN0, 0);
 
     // set GPIO0
     gpio_set_pin(PIN0);
